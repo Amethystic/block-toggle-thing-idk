0e1be9c599a8a5573dcc1736e95035e8c83ae4d5
Athena <119145022+Amethystic@users.noreply.github.com>
2024-03-06 02:38:54 -0600

    Create block.lua

---
+++ block.lua
@@ -0,0 +1,16 @@
+block = true
+-- piss code im still learn
+-- auuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuh
+
+function bounce()
+  print("Im updhhhhh uuugh boing boing aauuuhh")
+end
+
+if block then
+  print("Wake the fuck up")
+  while block do
+    bounce()
+  end
+else
+  print("its dead")
+end
